1a5a989781c77c5a11a8fdf58fa2df595466ed06
Adam PM <adam.pm77@gmail.com>
2022-09-30 15:43:03 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\BannerController;
-use App\Http\Controllers\Api\BookingController;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\BannerController;
+use App\Http\Controllers\API\BookingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
