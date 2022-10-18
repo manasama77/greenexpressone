3bf296bfc31e5f5d391796bb8e3f82f22691aa7f
Adam PM <adam.pm77@gmail.com>
2022-10-18 23:07:24 +0700

    perbaikan admin gila

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -28,45 +28,29 @@
                 </a>
             </li>
             <li class=" navigation-header">
-                <span data-i18n="Apps & Pages">Apps & Pages</span><i data-feather="more-horizontal"></i>
-            </li>
-            <li class=" nav-item">
-                <a class="d-flex align-items-center" href="/admin/banner">
-                    <i data-feather="image"></i><span class="menu-title text-truncate" data-i18n="banner">Banner</span>
-                </a>
+                <span data-i18n="Master Area">Master Area</span><i data-feather="more-horizontal"></i>
             </li>
             <li class=" nav-item">
                 <a class="d-flex align-items-center" href="/admin/master_area">
-                    <i data-feather="map"></i><span class="menu-title text-truncate" data-i18n="master area">Main
+                    <i data-feather="map"></i><span class="menu-title text-truncate" data-i18n="main area">Main
                         Area</span>
                 </a>
             </li>
             <li class=" nav-item">
                 <a class="d-flex align-items-center" href="/admin/master_sub_area">
-                    <i data-feather="map"></i><span class="menu-title text-truncate" data-i18n="master sub area">Master
-                        Sub
+                    <i data-feather="map"></i><span class="menu-title text-truncate" data-i18n="sub area">Sub
                         Area</span>
                 </a>
             </li>
             <li class=" nav-item">
                 <a class="d-flex align-items-center" href="/admin/master_special_area">
-                    <i data-feather="dollar-sign"></i><span class="menu-title text-truncate"
-                        data-i18n="master Special">Master
-                        Special
-                        Area</span>
+                    <i data-feather="dollar-sign"></i><span class="menu-title text-truncate" data-i18n="Special Area">
+                        Special Area</span>
                 </a>
             </li>
-            <li class=" nav-item">
-                <a class="d-flex align-items-center" href="/admin/pages">
-                    <i data-feather="file-text"></i><span class="menu-title text-truncate"
-                        data-i18n="master Special">Pages</span>
-                </a>
-            </li>
-            <li class=" nav-item">
-                <a class="d-flex align-items-center" href="/admin/voucher">
-                    <i data-feather="percent"></i><span class="menu-title text-truncate"
-                        data-i18n="master Special">Voucher</span>
-                </a>
+
+            <li class=" navigation-header">
+                <span data-i18n="Package">Package</span><i data-feather="more-horizontal"></i>
             </li>
             <li class=" nav-item">
                 <a class="d-flex align-items-center" href="/admin/charter">
@@ -80,15 +64,56 @@
                         data-i18n="master Special">Shuttles</span>
                 </a>
             </li>
-            <li class=" nav-item">
-                <a class="d-flex align-items-center" href="/admin/booking">
-                    <i data-feather="bookmark"></i><span class="menu-title text-truncate"
-                        data-i18n="booking">Bookings</span>
-                </a>
+
+            <li class=" navigation-header">
+                <span data-i18n="User Management">User Management</span><i data-feather="more-horizontal"></i>
             </li>
             <li class=" nav-item">
                 <a class="d-flex align-items-center" href="/admin/admin">
                     <i data-feather="users"></i><span class="menu-title text-truncate" data-i18n="admin">Admins</span>
+                </a>
+            </li>
+            <li class=" nav-item">
+                <a class="d-flex align-items-center" href="/admin/user">
+                    <i data-feather="users"></i><span class="menu-title text-truncate" data-i18n="admin">Users</span>
+                </a>
+            </li>
+
+            <li class=" navigation-header">
+                <span data-i18n="Transaction">Transaction</span><i data-feather="more-horizontal"></i>
+            </li>
+            <li class=" nav-item">
+                <a class="d-flex align-items-center" href="/admin/booking/shuttle">
+                    <i data-feather="bookmark"></i><span class="menu-title text-truncate" data-i18n="booking">Bookings
+                        Shuttle</span>
+                </a>
+            </li>
+            <li class=" nav-item">
+                <a class="d-flex align-items-center" href="/admin/booking/charter">
+                    <i data-feather="bookmark"></i><span class="menu-title text-truncate" data-i18n="booking">Bookings
+                        Charter</span>
+                </a>
+            </li>
+
+            <li class=" navigation-header">
+                <span data-i18n="Utilities">Utilities</span><i data-feather="more-horizontal"></i>
+            </li>
+            <li class=" nav-item">
+                <a class="d-flex align-items-center" href="/admin/banner">
+                    <i data-feather="image"></i><span class="menu-title text-truncate"
+                        data-i18n="banner">Banner</span>
+                </a>
+            </li>
+            <li class=" nav-item">
+                <a class="d-flex align-items-center" href="/admin/pages">
+                    <i data-feather="file-text"></i><span class="menu-title text-truncate"
+                        data-i18n="master Special">Pages</span>
+                </a>
+            </li>
+            <li class=" nav-item">
+                <a class="d-flex align-items-center" href="/admin/voucher">
+                    <i data-feather="percent"></i><span class="menu-title text-truncate"
+                        data-i18n="master Special">Voucher</span>
                 </a>
             </li>
         </ul>
